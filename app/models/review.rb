c4fe073651971291ef0be6dcd792d7a2d60e39d6
Arnaud Fiora <arnaud.fiora@gmail.com>
2019-10-30 15:54:50 +0100

    MVP finished with basic functions and design. No admin session

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,7 +5,7 @@ class Review < ApplicationRecord
   validates :content, :rating, presence: true
   validates :rating, numericality: { only_integer: true }
   validates :rating, inclusion: {
-    in: (0..5),
+    in: (0..5).to_a,
     message: 'Please enter a rating between 0 and 5'
   }
 end
